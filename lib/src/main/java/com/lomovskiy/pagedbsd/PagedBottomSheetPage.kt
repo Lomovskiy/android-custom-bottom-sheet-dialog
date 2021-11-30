3d52b513d8ae3da8f0ae9eaa9e5f9018871bcdbe
Alexander Lomovskiy <a.lomovskiy@eapteka.ru>
2021-11-30 10:23:07 +0300

    refactored collect state stream

--- lib/src/main/java/com/lomovskiy/pagedbsd/PagedBottomSheetPage.kt
+++ lib/src/main/java/com/lomovskiy/pagedbsd/PagedBottomSheetPage.kt
@@ -7,6 +7,7 @@ import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.createViewModelLazy
 import androidx.lifecycle.*
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlin.reflect.KClass
@@ -15,6 +16,8 @@ abstract class PagedBottomSheetPage<A, S, VM : PagedBottomSheetVM<A, S>>(
     @LayoutRes contentLayoutId: Int,
     viewModelClass: KClass<VM>
 ) : Fragment(contentLayoutId) {
+
+    private var collectStateStreamJob: Job? = null
 
     protected val vm: VM by createViewModelLazy(
         viewModelClass,
@@ -34,11 +37,18 @@ abstract class PagedBottomSheetPage<A, S, VM : PagedBottomSheetVM<A, S>>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallback)
-        viewLifecycleOwner.lifecycleScope.launch {
-            vm.getStateStream()
-                .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.RESUMED)
-                .collect(::renderState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        collectStateStreamJob = viewLifecycleOwner.lifecycleScope.launch {
+            vm.getStateStream().collect(::renderState)
         }
+    }
+
+    override fun onStop() {
+        collectStateStreamJob?.cancel()
+        super.onStop()
     }
 
     abstract fun onBackPressed()
